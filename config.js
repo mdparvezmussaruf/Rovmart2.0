e218c5b7c8a473c42068b1aeb87d44ad0838a1ff
MD Parvez Mussaruf <49514187+mdparvezmussaruf@users.noreply.github.com>
2026-09-25 19:28:58 +0600

    Update Google Apps Script URL in config.js

--- config.js
+++ config.js
@@ -1,4 +1,4 @@
 // Paste your deployed Google Apps Script Web App URL here.
 const CONFIG = Object.freeze({
-  GOOGLE_SCRIPT_URL: "https://script.google.com/macros/s/AKfycbx57bFvK8taeH5evmwn_Hq11dlj4mBXh83qtDPGEq78N87ZVufye4JvBXLcOnB0I_LgbQ/exec"
+  GOOGLE_SCRIPT_URL: "https://script.google.com/macros/s/AKfycbzzyPxWnCp25bLI9KxVOUWyDK4jAnGyfhtnFD2yyVi8tGhuDbW3QrQJrtiM_FxD3wof9A/exec"
 });
