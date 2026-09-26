7648de2098b03fca2279a64e827854e0980056c6
MD Parvez Mussaruf <49514187+mdparvezmussaruf@users.noreply.github.com>
2026-09-26 14:00:26 +0600

    Update GOOGLE_SCRIPT_URL in config.js

--- config.js
+++ config.js
@@ -1,6 +1,6 @@
 /* ROVMART frontend configuration */
 const CONFIG = Object.freeze({
-  GOOGLE_SCRIPT_URL: "https://script.google.com/macros/s/AKfycbzW7RBoFfw6UkGT5WMoOBdLtJU63z-OwBTRG6jA9sreG9FlxV9245PUYK5wkdlFY8pcwQ/exec",
+  GOOGLE_SCRIPT_URL: "https://script.google.com/macros/s/AKfycbwnsVrgr8QMmOjG-PF84KgxpUxUm-oBewOojJLKWDsG6InOyjcClAL30gezh0Eo6rmT6w/exec",
   CURRENCY_SYMBOL: "৳",
   DELIVERY_CHARGES: Object.freeze({ inside: 50, outside: 100 }),
   MAX_CART_QUANTITY: 99,
