0bc3b84bccfbed740b020fcbcddd9fe5f1eb16e0
MD Parvez Mussaruf <49514187+mdparvezmussaruf@users.noreply.github.com>
2026-09-26 11:24:03 +0600

    Delete config.js

--- config.js
+++
@@ -1,4 +0,0 @@
-// Paste your deployed Google Apps Script Web App URL here.
-const CONFIG = Object.freeze({
-  GOOGLE_SCRIPT_URL: "https://script.google.com/macros/s/AKfycbzzyPxWnCp25bLI9KxVOUWyDK4jAnGyfhtnFD2yyVi8tGhuDbW3QrQJrtiM_FxD3wof9A/exec"
-});
